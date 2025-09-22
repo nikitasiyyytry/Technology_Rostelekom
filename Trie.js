8947a16491d521ec1577e5cdfc06be85c4d2a746
nikitasiyyytry <knv07082005@gmail.com>
2025-09-22 13:12:46 +0500

    Add files via upload

---
+++ Trie.js
@@ -0,0 +1,66 @@
+/**
+ * Seminar 2.5 Simple Trie
+ */
+
+
+class TrieNode {
+    constructor(key) {
+        this.key = key;
+        this.children = {};
+        this.isWord = false;
+    }
+}
+
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode(null);
+    }
+
+    insert(word) {
+        // TODO Insert word symbol by symbol
+        let node = this.root;
+        for(let i=0; i<word.length; i++){
+            const symbol = word[i];
+            if(!node.children[symbol]){
+                node.children[symbol] = new TrieNode(symbol);
+            }
+
+            node = node.children[symbol];
+
+            if(i == word.length - 1){
+                node.isWord = true;
+            }
+        }
+    }
+
+    hasNode(word){
+        // TODO Check is word in Trie
+        let node = this.root;
+        for (let i = 0; i < word.length; i++) {
+            const symbol = word[i];
+            if (!node.children[symbol]) {
+                return false;
+            }
+            node = node.children[symbol];
+        }
+        return node.isWord;
+    }
+
+    getAllNodes(){
+        // TODO returns all nodes as array
+        const result = [];
+
+        function dfs(node) {
+            result.push(node);
+            for (const child of Object.values(node.children)) {
+                dfs(child);
+            }
+        }
+
+        dfs(this.root);
+        return result;
+    }
+}
+
+module.exports = { Trie };
