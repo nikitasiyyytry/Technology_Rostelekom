72a6ea4c8cad5062ab9654410d6391964b773d92
nikitasiyyytry <knv07082005@gmail.com>
2025-10-15 12:36:44 +0500

    Add files via upload

--- Blockchain.js
+++ Blockchain.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Seminar 2.1 Blockchain primitive
  */
@@ -49,55 +48,3 @@ class Blockchain {
 }
 
 module.exports = { Block, Blockchain };
-=======
-/**
- * Seminar 2.1 Blockchain primitive
- */
-
-const SHA256 = require('ethereum-cryptography/sha256').sha256;
-const utf8ToBytes = require('ethereum-cryptography/utils').utf8ToBytes;
-
-
-class Block {
-    constructor(data){
-        this.data = data;      // Here we simplify data, let it be just a simple string
-        this.previousHash = null;
-    }
-
-    toHash(){
-        const hashBytes = utf8ToBytes(this.data + this.previousHash);
-        return SHA256(hashBytes);        // a hash as byte array
-    }
-}
-
-
-class Blockchain {
-    constructor() {
-        
-        this.chain = [
-            new Block("Hello Blockchain!")
-            ];
-    }
-
-    addBlock(block){
-        // TODO 2 Compute block.previousHash = previousBlock.toHash()
-        block.previousHash = this.chain[this.chain.length - 1].toHash()
-        this.chain.push(block)
-        
-    }
-
-    isValid(){
-        // TODO 3 Check every block previous hash
-        for (let i=this.chain.length - 1; i>0; i--){
-            const block = this.chain[i];
-            const previous_block = this.chain[i - 1];
-            if (block.previousHash.toString() !== previous_block.toHash().toString()){
-                return false;
-            }
-        }
-        return true;
-    }
-}
-
-module.exports = { Block, Blockchain };
->>>>>>> 8843b94638c214114c7ab52e32cefee6ca7d3349
--- Trie.js
+++ Trie.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Seminar 2.5 Simple Trie
  */
@@ -65,71 +64,3 @@ class Trie {
 }
 
 module.exports = { Trie };
-=======
-/**
- * Seminar 2.5 Simple Trie
- */
-
-
-class TrieNode {
-    constructor(key) {
-        this.key = key;
-        this.children = {};
-        this.isWord = false;
-    }
-}
-
-
-class Trie {
-    constructor() {
-        this.root = new TrieNode(null);
-    }
-
-    insert(word) {
-        // TODO Insert word symbol by symbol
-        let node = this.root;
-        for(let i=0; i<word.length; i++){
-            const symbol = word[i];
-            if(!node.children[symbol]){
-                node.children[symbol] = new TrieNode(symbol);
-            }
-
-            node = node.children[symbol];
-
-            if(i == word.length - 1){
-                node.isWord = true;
-            }
-        }
-    }
-
-    hasNode(word){
-        // TODO Check is word in Trie
-        let node = this.root;
-        for (let i = 0; i < word.length; i++) {
-            const symbol = word[i];
-            if (!node.children[symbol]) {
-                return false;
-            }
-            node = node.children[symbol];
-        }
-        return node.isWord;
-    }
-
-    getAllNodes(){
-        // TODO returns all nodes as array
-        const result = [];
-
-        function dfs(node) {
-            result.push(node);
-            for (const child of Object.values(node.children)) {
-                dfs(child);
-            }
-        }
-
-        dfs(this.root);
-        return result;
-    }
-}
-
-module.exports = { Trie };
->>>>>>> 8843b94638c214114c7ab52e32cefee6ca7d3349
